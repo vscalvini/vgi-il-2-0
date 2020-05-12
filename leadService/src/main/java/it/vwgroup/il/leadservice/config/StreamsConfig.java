4920ef69d58867246646f0437bde97faae45b3f4
Nicola Gaffurini <nicola.gaffurini@xeos.it>
2020-05-12 18:01:07 +0200

    Push modifiche leadService

--- leadService/src/main/java/it/vwgroup/il/leadservice/config/StreamsConfig.java
+++ leadService/src/main/java/it/vwgroup/il/leadservice/config/StreamsConfig.java
@@ -1,9 +1,36 @@
 package it.vwgroup.il.leadservice.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+import feign.RequestInterceptor;
 import it.vwgroup.il.leadservice.stream.LeadStreams;
 
+@Configuration
+@EnableResourceServer
 @EnableBinding(LeadStreams.class)
-public class StreamsConfig {
+public class StreamsConfig extends ResourceServerConfigurerAdapter{
+
+	@Bean
+    @ConfigurationProperties(prefix = "security.oauth2.client")
+    public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(){
+        return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
+    }
+
+    @Bean
+    public OAuth2RestTemplate clientCredentialsRestTemplate() {
+        return new OAuth2RestTemplate(clientCredentialsResourceDetails());
+    }
 }
