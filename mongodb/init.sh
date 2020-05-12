5a896d8a9e96468679784f14e2ddd8e8bce54af3
vscalvin <valter.scalvini@eng.it>
2020-05-12 15:52:31 +0200

    nuovo repo

---
+++ mongodb/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if test -z "$MONGODB_PASSWORD"; then
+    echo "MONGODB_PASSWORD not defined"
+#    exit 1
+fi
+
+auth="-u user -p $MONGODB_PASSWORD"
+
+# MONGODB USER CREATION
+(
+echo "setup mongodb auth"
+create_user="if (!db.getUser('user')) { db.createUser({ user: 'user', pwd: 'password', roles: [ {role:'readWrite', db:'vgi_il'} ]}) }"
+until mongo vgi_il --eval "$create_user" || mongo vgi_il $auth --eval "$create_user"; do sleep 5; done
+killall mongod
+sleep 1
+killall -9 mongod
+) &
+
+# INIT DUMP EXECUTION
+(
+if test -n "$INIT_DUMP"; then
+    echo "execute dump file"
+	until mongo vgi_il $auth $INIT_DUMP; do sleep 5; done
+fi
+) &
+
+echo "start mongodb without auth"
+chown -R mongodb /data/db
+gosu mongodb mongod "$@"
+
+echo "restarting with auth on"
+sleep 5
+exec gosu mongodb /usr/local/bin/docker-entrypoint.sh --auth "$@"
