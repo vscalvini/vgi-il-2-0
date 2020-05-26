84b3b99883023ff35d4771dda4ee94b67ff85c41
vscalvin <valter.scalvini@eng.it>
2020-05-26 16:35:27 +0200

    first release

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -16,14 +16,6 @@ sleep 1
 killall -9 mongod
 ) &
 
-# INIT DUMP EXECUTION
-(
-if test -n "$INIT_DUMP"; then
-    echo "execute dump file"
-	until mongo vgi_il $auth $INIT_DUMP; do sleep 5; done
-fi
-) &
-
 echo "start mongodb without auth"
 chown -R mongodb /data/db
 gosu mongodb mongod "$@"
